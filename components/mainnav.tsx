c9d06aec7613e6337c3974cec8b84cdc7f6448d8
Trivial-lore <triviallore@gmail.com>
2023-08-21 08:39:05 -0500

    setting up navbar

---
+++ components/mainnav.tsx
@@ -0,0 +1,8 @@
+const MainNav = () => {
+  return (
+    <div>
+      Main Nav
+    </div>
+  );
+}
+export default MainNav;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,18 @@
+import Container from "@/components/ui/container";
+import Link from "next/link";
+import MainNav from "@/components/mainnav";
+
 const Navbar = () => {
   return (
-    <nav>
-      Navbar
+    <nav className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">STORE</p>
+          </Link>
+          <MainNav/>
+        </div>
+      </Container>
     </nav>
   );
 }
