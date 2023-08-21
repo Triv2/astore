32e037816b3bfeb572f216e59f6de90f2feb8608
Trivial-lore <triviallore@gmail.com>
2023-08-21 08:50:40 -0500

    added tailwind-merge

--- components/mainnav.tsx
+++ components/mainnav.tsx
@@ -1,8 +1,36 @@
-const MainNav = () => {
+'use client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation"
+
+interface MainNavProps {
+  data : any
+};
+
+const MainNav:React.FC<MainNavProps> = ({
+  data
+}) => {
+
+  const pathname = usePathname();
+  const routes = data.map((route) => ({
+    href:`/category${route.id}`,
+    label:route.name,
+    active: pathname === `/category${route.id}`
+  }))
+
+
   return (
-    <div>
-      Main Nav
-    </div>
+    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
+      {routes.map((route)=>(
+        <Link
+        key={route.href}
+        href={route.href}
+        className={}
+        >
+
+        </Link>
+      ))}
+    </nav>
   );
 }
 export default MainNav;
