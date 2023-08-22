96fdef53b30d07a90215630dffd50d5dcaf77324
Trivial-lore <triviallore@gmail.com>
2023-08-21 20:18:48 -0500

    setup types for recieving data from the server

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,17 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard= await getBillboard("1f5d23a8-bd00-4e22-b6f9-17ba22899291");
   return (
-    <div>Hello Store</div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   );
 }
 
