a5f99b24696f3686935f07663d25180f7f07c858
Trivial-lore <triviallore@gmail.com>
2023-08-23 06:19:18 -0500

    cart functionality added

---
+++ providers/toast-provider.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import {Toaster} from "react-hot-toast";
+
+interface ToastProviderProps {}
+
+const ToastProvider = () => {
+  return (
+    <Toaster/>
+  );
+}
+export default ToastProvider;
