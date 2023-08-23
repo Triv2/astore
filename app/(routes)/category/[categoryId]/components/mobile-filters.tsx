51cb088a565043e33d9fecc1f97249346ac5f143
Trivial-lore <triviallore@gmail.com>
2023-08-22 21:19:54 -0500

    mobile filters section finished

--- app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -2,8 +2,11 @@
 
 import Button from "@/components/ui/button";
 import { Color, Size } from "@/types";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { useState, useEffect } from "react";
+import { Dialog } from "@headlessui/react";
+import IconButton from "@/components/ui/icon-button";
+import Filter from "./filter";
 
 interface MobileFiltersProps {
   sizes: Size[];
@@ -25,6 +28,32 @@ const MobileFilters:React.FC<MobileFiltersProps> = ({
       Filters
       <Plus size= {20}/>
     </Button>
+    <Dialog open={open} as="div" className="relative z-40 lg:hidden" onClose={onClose}>
+      {/* {Background} */}
+      <div className="fixed inset-0 bg-black bg-opacity-25"/>
+      {/* {Dialog position} */}
+      <div className="fixed inset-0 z-40">
+        <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+          {/* {Close Button} */}
+          <div className="flex items-center justify-end px-4">
+            <IconButton icon={<X size={15}/>} onClick={onClose}/>
+          </div>
+          {/* {Filters} */}
+          <div className="p-4">
+            <Filter
+              valueKey="sizeId"
+              name="Sizes"
+              data={sizes}
+            />
+            <Filter
+              valueKey="colorId"
+              name="Colors"
+              data={colors}
+            />
+          </div>
+        </Dialog.Panel>
+      </div>
+    </Dialog>
     </>
   );
 }
